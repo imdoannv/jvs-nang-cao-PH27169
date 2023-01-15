6bd10e25fdcffbe32354e48c5535948c0dad1b17
Nguyễn Đoàn <nguyendoanjp12@gmail.com>
2023-01-16 00:51:37 +0700

    finish video 110 s9

--- s9/107-110/script.js
+++ s9/107-110/script.js
@@ -40,36 +40,107 @@ const restaurant = {
     }
 };
 
-// Video 109 s9
-const rest1 = {
-    name: 'Capri',
-    // numGuests: 20,
-    numGuests: 0,
+// Video 110 s9
+const game = {
+    team1: "Bayern Munich",
+    team2: "Borrussia Dormund",
+    players: [
+        [
+            "Neuer",
+            "Pavard",
+            "Martinez",
+            "Alaba",
+            "Davies",
+            "Kimmich",
+            "Goretzka",
+            "Coman",
+            "Muller",
+            "Gnarby",
+            "Lewandowki",
+        ],
+        [
+            "Burki",
+            "Schulz",
+            "Hummels",
+            "Akanji",
+            "Hakimi",
+            "Weigl",
+            "Wisel",
+            "Hazad",
+            "Brand",
+            "Sacnchi",
+            "Gotze",
+        ],
+    ],
+    score: "4:0",
+    scored: ["Lewandowki", "Ganrby", "Lewandowki", "Hummels"],
+    date: "Nov 9th 2037",
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
+    },
 };
-const rest2 = {
-    name: 'LaPiazza',
-    owner: 'Giovanni Rossi',
-}
 
-// OR assignment operator
-// rest1.numGuests = rest1.numGuests || 10;
-// rest2.numGuests = rest2.numGuests || 10;
+// 1.
+const [players1, players2] = game.players;
+console.log(players1, players2);
+//  2.
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
 
-// rest1.numGuests ||= 10;
-// rest2.numGuests ||= 10;
+// 3.
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
 
-// nullish assignment operator(null or underfined)
-// rest1.numGuests ??= 10;
-// rest2.numGuests ??= 10;
+//  4.
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Periscic'];
 
-// AND assignment operator
-// rest1.owner = rest1.owner && '<ANONYMOUS>';
-// rest2.owner = rest2.owner && '<ANONYMOUS>';
-rest1.owner &&= '<ANONYMOUS>';
-rest2.owner &&= '<ANONYMOUS>';
+// 5.
+const {odds: {team1, x: draw, team2},} = game;
+console.log(team1,draw,team2);
 
-console.log(rest1);
-console.log(rest2);
+// 6.
+const printGoals = function (...players){
+    console.log(players);
+    console.log(`${players.length} goals were scored`);
+};
+printGoals(...game.scored);
+
+// 7.
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
+
+// Video 109 s9
+// const rest1 = {
+//     name: 'Capri',
+//     // numGuests: 20,
+//     numGuests: 0,
+// };
+// const rest2 = {
+//     name: 'LaPiazza',
+//     owner: 'Giovanni Rossi',
+// }
+
+// // OR assignment operator
+// // rest1.numGuests = rest1.numGuests || 10;
+// // rest2.numGuests = rest2.numGuests || 10;
+
+// // rest1.numGuests ||= 10;
+// // rest2.numGuests ||= 10;
+
+// // nullish assignment operator(null or underfined)
+// // rest1.numGuests ??= 10;
+// // rest2.numGuests ??= 10;
+
+// // AND assignment operator
+// // rest1.owner = rest1.owner && '<ANONYMOUS>';
+// // rest2.owner = rest2.owner && '<ANONYMOUS>';
+// rest1.owner &&= '<ANONYMOUS>';
+// rest2.owner &&= '<ANONYMOUS>';
+
+// console.log(rest1);
+// console.log(rest2);
 
 // Video 108 s9
 
