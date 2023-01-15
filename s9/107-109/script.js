b2a3f06b5a0aec773560b93e010f24c3704d1126
Nguyễn Đoàn <nguyendoanjp12@gmail.com>
2023-01-16 00:25:38 +0700

    finish 109 s9

--- s9/107-109/script.js
+++ s9/107-109/script.js
@@ -40,51 +40,84 @@ const restaurant = {
     }
 };
 
-// Video 107 s9
-
-console.log('---------------- OR --------------');
-
-// Use any data type, return Any data type, short-circuiting
-console.log(3 || 'Jona');
-console.log('' || 'Jona');
-console.log(true || 0);
-console.log(undefined || null);
-
-console.log(undefined || 0 || '' || 'Hello' || 23 || null);
-
-
-restaurant.numGuests = 23;
-const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
-console.log(guests1);
-
-const guests2 = restaurant.numGuests || 10;
-console.log(guests2);
-
-console.log('---------------- AND --------------');
-console.log(0 && 'Jonas');
-console.log(7 && 'Jonas');
-
-console.log('Hello' && 23 && null && 'Jonas');
-
-// Ví dụ thực tế Practical example
-if(restaurant.orderPizza){
-    restaurant.orderPizza('mushrooms', 'spinach');
+// Video 109 s9
+const rest1 = {
+    name: 'Capri',
+    // numGuests: 20,
+    numGuests: 0,
+};
+const rest2 = {
+    name: 'LaPiazza',
+    owner: 'Giovanni Rossi',
 }
 
-restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+// OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// nullish assignment operator(null or underfined)
+// rest1.numGuests ??= 10;
+// rest2.numGuests ??= 10;
+
+// AND assignment operator
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
+
+// Video 108 s9
+
+// restaurant.numGuests = 0;
+// const guests = restaurant.numGuests || 10;
+// console.log(guests);
+
+// // Nulish: null and underfined (NOT 0 or '')
+// const guestCorrect = restaurant.numGuests ?? 10;
+// console.log(guestCorrect);
+
+// Video 107 s9
+
+// console.log('---------------- OR --------------');
+
+// // Use any data type, return Any data type, short-circuiting
+// console.log(3 || 'Jona');
+// console.log('' || 'Jona');
+// console.log(true || 0);
+// console.log(undefined || null);
+
+// console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+
+// restaurant.numGuests = 23;
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// console.log(guests1);
+
+// const guests2 = restaurant.numGuests || 10;
+// console.log(guests2);
+
+// console.log('---------------- AND --------------');
+// console.log(0 && 'Jonas');
+// console.log(7 && 'Jonas');
+
+// console.log('Hello' && 23 && null && 'Jonas');
+
+// // Ví dụ thực tế Practical example
+// if(restaurant.orderPizza){
+//     restaurant.orderPizza('mushrooms', 'spinach');
+// }
+
+// restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
 
 
 // Toán tử OR sẽ trả về giá trị trung thực đâu tiên của tất cả các toán hạng hoặc đơn giản là giá trị cuối cùng nếu tất cả giá trị của chúng đều sai(Dùng OR để đặt các giá trị mặc định)
 // Toán tử AND sẽ trả về giá trị falsy đầu tiên hoặc giá trị cuối cùng nếu tất cả chúng đều true(Dùng AND để thực thi mã trong toán hạng thứ 2 nếu toán hạng đầu tiên đúng)
 
 
-// Video 108 s9
 
-restaurant.numGuests = 0;
-const guests = restaurant.numGuests || 10;
-console.log(guests);
-
-// Nulish: null and underfined (NOT 0 or '')
-const guestCorrect = restaurant.numGuests ?? 10;
-console.log(guestCorrect);
 
