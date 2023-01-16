c1da50f9469f0a759e161f9f4bb839470d0bcb9f
Nguyễn Đoàn <nguyendoanjp12@gmail.com>
2023-01-17 00:48:41 +0700

    finish video 125 - finish s9

--- s9/107-125/script.js
+++ s9/107-125/script.js
@@ -57,24 +57,46 @@ const restaurant = {
         console.log(otherInggredients);
     },
 };
-// // // Video 124 s9
-document.body.append(document.createElement('textarea'));
-document.body.append(document.createElement('button'));
+// // // // Video 125 s9 - Video fisnh s9
+const flights = 
+'_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Deparure;fao93766109;lis2323639855;12:30';
+
+// console.log(flights.split('+'));
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')){
+    const [type, from, to, time] = flight.split(';');
+    const output = `${type.startsWith('_Delayed') ? 'X' : ''}${type.replaceAll(
+        '_',
+        ' '
+    )} ${getCode(from)} ${getCode(to)} (${time.replace(':','h')})`.padStart(36);
+    console.log(output);
+}
 
 
-document.querySelector('button').addEventListener('click', function(){
-    const text = document.querySelector('textarea').value;
-    const rows = text.split('\n');
-    for(const [i, row] of rows.entries){
-        const [first, second] = row.toLowerCase().trim().split('_');
+
+
+
+
+// // // // Video 124 s9
+// document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
+
+
+// document.querySelector('button').addEventListener('click', function(){
+//     const text = document.querySelector('textarea').value;
+//     const rows = text.split('\n');
+//     for(const [i, row] of rows.entries){
+//         const [first, second] = row.toLowerCase().trim().split('_');
         
-        const output = `${first}${second.replace(
-            second[0],
-            second[0].toUpperCase()
-        )}`;
-        console.log(`${output.padEnd(20)} ${'<3'.repeat(i+1)} `);
-    }
-});
+//         const output = `${first}${second.replace(
+//             second[0],
+//             second[0].toUpperCase()
+//         )}`;
+//         console.log(`${output.padEnd(20)} ${'<3'.repeat(i+1)} `);
+//     }
+// });
 
 
 
