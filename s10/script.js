7d847bacaa99f6da5395185e372bca6b7ec58868
Nguyễn Đoàn <nguyendoanjp12@gmail.com>
2023-01-17 13:56:08 +0700

    finish video 128 s10

---
+++ s10/script.js
@@ -0,0 +1,27 @@
+// // Video 128 s10
+
+
+'use strict';
+
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers = 1, price = 199 * numPassengers){
+    // ES5
+    // numPassengers = numPassengers || 1;
+    // price = price || 199;
+    const booking = {
+        flightNum,
+        numPassengers,
+        price,
+    };
+    console.log(booking);
+    bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123',2);
+createBooking('LH123',5);
+
+//undefined bỏ qua hành khách mặc định thành giá trị đầu là 1
+createBooking('LH123', undefined, 100);
