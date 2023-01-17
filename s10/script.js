52fcd0c7c3a283da346f74be2e3aa6bce4228f7a
Nguyễn Đoàn <nguyendoanjp12@gmail.com>
2023-01-17 14:16:00 +0700

    finish video 129 s10

--- s10/script.js
+++ s10/script.js
@@ -1,27 +1,61 @@
-// // Video 128 s10
+// // // Video 128 s10
 
 
 'use strict';
 
-const bookings = [];
+// const bookings = [];
 
-const createBooking = function (flightNum, numPassengers = 1, price = 199 * numPassengers){
-    // ES5
-    // numPassengers = numPassengers || 1;
-    // price = price || 199;
-    const booking = {
-        flightNum,
-        numPassengers,
-        price,
-    };
-    console.log(booking);
-    bookings.push(booking);
+// const createBooking = function (flightNum, numPassengers = 1, price = 199 * numPassengers){
+//     // ES5
+//     // numPassengers = numPassengers || 1;
+//     // price = price || 199;
+//     const booking = {
+//         flightNum,
+//         numPassengers,
+//         price,
+//     };
+//     console.log(booking);
+//     bookings.push(booking);
+// };
+
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// createBooking('LH123',2);
+// createBooking('LH123',5);
+
+// //undefined bỏ qua hành khách mặc định thành giá trị đầu là 1
+// createBooking('LH123', undefined, 100);
+
+// // Video 129 s10
+
+const flight = 'LH234';
+const jonas = {
+    name : 'Doanf Nguyeenx',
+    passport : 24739479284,
+}
+
+const checkIn = function(flightNum, passenger){
+    flightNum = 'LH999';
+    passenger.name = 'Mr. '+passenger.name;
+
+    if(passenger.passport === 24739479284){
+        alert('Checked in');
+    }else{
+        alert('Wrong passport!');
+    }
 };
 
-createBooking('LH123');
-createBooking('LH123', 2, 800);
-createBooking('LH123',2);
-createBooking('LH123',5);
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
 
-//undefined bỏ qua hành khách mặc định thành giá trị đầu là 1
-createBooking('LH123', undefined, 100);
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function(person){
+    person.passport = Math.trunc(Math.random() * 10000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
