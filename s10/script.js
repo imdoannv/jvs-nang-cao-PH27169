71fc55713d76c8e0c61c13b494bfa7055d3c577f
Nguyễn Đoàn <nguyendoanjp12@gmail.com>
2023-01-17 14:42:10 +0700

    finish video 131 s10

--- s10/script.js
+++ s10/script.js
@@ -28,34 +28,70 @@
 
 // // Video 129 s10
 
-const flight = 'LH234';
-const jonas = {
-    name : 'Doanf Nguyeenx',
-    passport : 24739479284,
+// const flight = 'LH234';
+// const jonas = {
+//     name : 'Doanf Nguyeenx',
+//     passport : 24739479284,
+// }
+
+// const checkIn = function(flightNum, passenger){
+//     flightNum = 'LH999';
+//     passenger.name = 'Mr. '+passenger.name;
+
+//     if(passenger.passport === 24739479284){
+//         alert('Checked in');
+//     }else{
+//         alert('Wrong passport!');
+//     }
+// };
+
+// // checkIn(flight, jonas);
+// // console.log(flight);
+// // console.log(jonas);
+
+// // Is the same as doing...
+// // const flightNum = flight;
+// // const passenger = jonas;
+
+// const newPassport = function(person){
+//     person.passport = Math.trunc(Math.random() * 10000000000);
+// };
+
+// newPassport(jonas);
+// checkIn(flight, jonas);
+
+
+
+
+
+// // Video 131 s10
+
+const oneWord = function(str){
+    return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function(str){
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+};
+
+//Higher-order function 
+const transformer = function(str, fn){
+    console.log(`Original string: ${str}`);
+    console.log(`Transformed string: ${fn(str)}`);
+
+    console.log(`Transformed string: ${fn.name}`);
+};
+
+transformer('JavaScipt is the best!', upperFirstWord);
+transformer('JavaScipt is the best!', oneWord);
+
+
+// Js uses callbacks all the time -> gọi mói lúc
+const hight5 = function(){
+    console.log('<3');
 }
 
-const checkIn = function(flightNum, passenger){
-    flightNum = 'LH999';
-    passenger.name = 'Mr. '+passenger.name;
+document.body.addEventListener('click', hight5);
 
-    if(passenger.passport === 24739479284){
-        alert('Checked in');
-    }else{
-        alert('Wrong passport!');
-    }
-};
-
-// checkIn(flight, jonas);
-// console.log(flight);
-// console.log(jonas);
-
-// Is the same as doing...
-// const flightNum = flight;
-// const passenger = jonas;
-
-const newPassport = function(person){
-    person.passport = Math.trunc(Math.random() * 10000000000);
-};
-
-newPassport(jonas);
-checkIn(flight, jonas);
+['Jonas', 'Martha', 'Doan'].forEach(hight5);
