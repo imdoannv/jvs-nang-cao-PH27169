3e9d5a575c43fc7cab4a43cf381ac1b99c238d54
Nguyễn Đoàn <nguyendoanjp12@gmail.com>
2023-02-11 13:06:44 +0700

    finish video 193 - finish s13

--- s13/script.js
+++ s13/script.js
@@ -233,3 +233,30 @@ document.addEventListener('keydown', function (e) {
 //   this.style.backgroundColor = randomColor();
 //   console.log('NAV', e.target, e.currentTarget);
 // });
+
+
+// // // // // // // // // Video 193 s13
+ const h1 = document.querySelector('h1');
+
+// Going downards: child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+h1.closest('.header').style.background = 'var(--gradient-secondry)';
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+//Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el){
+  if (el !== h1) el.style.transfrom = 'scale(0.5)';
+});
